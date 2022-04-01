bdf99576d572237c684eec89125a366e566feb21
AEbeckNet <102826584+AEbeckNet@users.noreply.github.com>
2022-04-02 01:17:19 +0700

    Add files via upload
    Sc Sederhana yang merekomendasikan web dengan mudaj, no ribet semoga bermanfaat....

---
+++ ebeck1.py
@@ -0,0 +1,52 @@
+#!/bin/user/python2
+#Ebeck Kansas Real
+#Sc Sederhana Mengerekomendasikan Web Yang Mungkin Berguna
+#Semoga Bermanfaat
+
+import os
+import sys
+import time
+
+os.system("xdg-open https://youtube.com/channel/UCJxbMJ75KHZgTDFfP2TpFHg")
+os.system("clear")
+def bersih():
+    os.system("clear")
+
+#########LOGO#########
+
+print (  """
+
+
+
+                \033[1;34m___________________________________________\033[1;34m 
+                \033[1;34m|\033[1;34m \033[1;0mATHOR\033[1;0m       :  \033[1;0mMR./EBECK_KANSAS_REAL\033[1;0m     \033[1;34m|\033[1;34m
+                \033[1;34m|\033[1;34m \033[1;0mVERSION SC\033[1;0m  :  \033[1;0m0.1\033[1;0m                       \033[1;34m|\033[1;34m
+                \033[1;34m|\033[1;34m \033[1;0mFOR        :  \033[1;0m WEB TOOLS\033[1;0m                 \033[1;34m|\033[1;34m
+                \033[1;34m|\033[1;34m\033[1;34m___________\033[1;34m \033[1;0mGhotsInternet\033[1;0m \033[1;34m________________\033[1;34m\033[1;34m|
+""")
+
+def menu():
+    print("1. Lacak Lokasi No Hp")
+    print("2. Keyloger")
+    print("3. Tambah Followers Instagram")
+    print("4. Download Vidio Semua Social Media")
+    print("5. Exit")
+    # input
+    pilih = input("Pilih: ")
+    if pilih ==1:
+        os.system("xdg-open https://ceebydith.com/cek-hlr-lokasi-hp.html")
+    if pilih ==2:
+        os.system("xdg-open https://www.malavida.com/en/soft/keylogger/android/")
+    if pilih ==3:
+        os.system("xdg-open https://viakep.com/instagram-takipci-hilesi/")
+    if pilih ==4:
+        os.system("xdg-open https://id.savefrom.net/91/")
+    if pilih ==5:
+        os.system("exit")
+        os.system("sleep2") 
+        print "Terima Kasih Telah Menggunakan Tolls Saya..."
+
+
+
+menu()
+bersih()
